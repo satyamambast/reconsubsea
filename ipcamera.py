8634499c64bfdf101acfab1a5d037af357f9ea8c
satyamambast <satyam_ambast@srmuniv.edu.in>
2019-04-06 22:09:19 +0530

    sh

--- ipcamera.py
+++ ipcamera.py
@@ -1,10 +1,13 @@
 import cv2
 
-cap = cv2.VideoCapture('rtsp://admin:@169.254.99.116/user=admin&password=&channel=1&stream=0.sdp?')
+cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(1)
 
 while(True):
     ret, frame = cap.read()
+    ret1, frame1 = cap1.read()
     cv2.imshow('frame',frame)
+    cv2.imshow('frame1',frame1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
